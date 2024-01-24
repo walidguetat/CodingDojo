27f141ba6c90dce723e3b0f17d46cb9b471b5763
walidguetat <gtetwalid@yahoo.fr>
2024-01-24 12:03:32 +0100

    Assimgnent code.js

---
+++ preBootcamp/code.js/code.js
@@ -0,0 +1,8 @@
+// on insialized this is varriable likecount of zero 
+var likecount = 0;
+// on create a function increaselikes for calculate or sum  numbers at likes for flowers 
+function increaselikes() {
+    // on increment one at this varriable likecount
+    likecount = likecount + 1;
+}
+
