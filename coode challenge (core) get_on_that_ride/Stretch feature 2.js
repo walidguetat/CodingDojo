cd0471322f1a2edf08580db9ae807bbbb800dec8
walidguetat <gtetwalid@yahoo.fr>
2024-01-24 21:04:07 +0100

    code challenge (core) get_on_that_ride

---
+++ coode challenge (core) get_on_that_ride/Stretch feature 2.js
@@ -0,0 +1,16 @@
+// code challange get on that ride
+
+
+// Stretch feature 2
+
+// on intillized the varriable height of kids is 10
+var heigth = 10;
+// on intiallized the varriable age of kids is  15
+var age = 15;
+// on testing if your height is a greater than 42  or your age is a greater than 15
+if (heigth >= 42 || age >= 10) {
+    console.log("Get on that Ride, Kiddo"); // print this message 'Get on  that ride, Kiddo ' in screen
+}
+else {
+    console.log("Sorry Kiddo, Maybe next year ! ");// print this message 'sorry Kiddo, Maybe next year !'
+}
---
+++ coode challenge (core) get_on_that_ride/assignemnt stretch.js
@@ -0,0 +1,11 @@
+// code challange get on that ride
+
+// on intillized the height of kids of 42
+var heigth = 10;
+// on testing if your height is a greater than 42 
+if (heigth >= 42) {
+    console.log("Get on that Ride, Kiddo"); // print this message 'Get on  that ride, Kiddo ' in screen
+}
+else {
+    console.log("Sorry Kiddo, Maybe next year ! ");// print this message 'sorry Kiddo, Maybe next year !'
+}
