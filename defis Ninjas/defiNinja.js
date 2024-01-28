b3ee9063ef3740c4f6c8a340085c3041321b233a
walidguetat <gtetwalid@yahoo.fr>
2024-01-28 18:02:42 +0100

    Defi Ninja

---
+++ defis Ninjas/defiNinja.js
@@ -0,0 +1,26 @@
+// 
+
+
+
+// Question 1 : How would you get the character to move up?
+
+// Ansewr 1 : 
+// ----------------------------------------------------------------------------------
+
+// To change the direction of the character,
+// we change the variables from right to left and from north to south
+
+// ----------------------------------------------------------------------------------
+
+// Question 2 : The character can currently go everywhere, 
+//              even where leftValue is negative or topValue is negative. 
+//              How would you adjust the code above so that the character stays between 0 to 500 
+//              for both the leftValue and the topValue variables? 
+
+
+// Ansewr 2 : 
+// ----------------------------------------------------------------------------------
+
+//  leftvalue  :   add the condition to move and to stop between [0 .. 500]
+//  topvalue   :   add the condition to move and to stop between [0 .. 500]
+
