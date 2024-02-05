f02f744e889774ffdee806a75c998f5e24e9dcc3
walidguetat <gtetwalid@yahoo.fr>
2024-02-05 14:52:47 +0100

    Button clicker

---
+++ Web Fundamentals/week_1/Day_5/Core/Button clicker/button_clicker.js
@@ -0,0 +1,11 @@
+function login_logout(element){
+    element.innerText="Log Out";
+}
+
+function remove_definiton(element){
+    element.remove();
+}
+
+function message(){
+    alert("Ninja was liked ! ");
+}
