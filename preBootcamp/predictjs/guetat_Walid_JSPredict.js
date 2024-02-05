2a32ffd0b1aba7db6421b51f3275e51b1008796a
walidguetat <gtetwalid@yahoo.fr>
2024-02-05 09:11:42 +0100

    Predict js

---
+++ preBootcamp/predictjs/guetat_Walid_JSPredict.js
@@ -0,0 +1,22 @@
+
+
+// Predict 1 
+
+//   print message : print I was born in 1980
+
+
+// Predict 2 
+
+//   print message :  I was born in 1980
+
+// Predict 3  
+
+//   If var num1 = 10 and var num2 = 20
+
+// print list of message and result of sum two numbers 
+
+// Summing Numbers !
+// num 1 is 10
+// num 2 is 20
+// 30
+
