42b8e61bb2dc6701604a9604a85cda272e632914
walidguetat <gtetwalid@yahoo.fr>
2024-02-08 16:28:23 +0100

    commit

---
+++ Web Fundamentals/week_2/Day_1/Practice/Video preview/script.js
@@ -0,0 +1,11 @@
+console.log("page loaded...");
+
+function play(){
+    var x=getElementById("#myvideo");
+    x.play();
+}
+
+function pause(){
+    var x=getElementById("#myvideo");
+    x.pause();
+}
