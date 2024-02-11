4f7ad6030547b8471af0c2073002da4a592ed188
walidguetat <gtetwalid@yahoo.fr>
2024-02-11 10:49:46 +0100

    This time ist

---
+++ this time its/thistimeits.js
@@ -0,0 +1,15 @@
+
+
+function  displayIfChildIsAbleToRideTheRollerCoaster(childHeight){
+
+    if(childHeight>52)
+    {
+        console.log("Get on that ride, Kiddo ! ");
+    }
+    else
+    {
+        console.log("Sorry Kiddo.Maybe next year.");
+    }
+}
+
+displayIfChildIsAbleToRideTheRollerCoaster(56);
