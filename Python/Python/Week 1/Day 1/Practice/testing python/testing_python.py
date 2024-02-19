3ae047cb2d12531fce39850d1f5ddcd385cf3703
walidguetat <gtetwalid@yahoo.fr>
2024-02-19 21:56:53 +0100

    hello_world

---
+++ Python/Python/Week 1/Day 1/Practice/testing python/testing_python.py
@@ -0,0 +1,77 @@
+# import random
+
+# print('Welcome to Python!')
+
+# print('This is a loop printing 5 times')
+# for x in range(1, 6):
+#     print(f'x is: {x}')
+
+# weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+# day = random.choice(weekdays)
+
+# print(f'Today is: {day}')
+
+# if day == 'Monday':
+#     print('It will be a long week!')
+# elif(day == 'Friday'):
+#     print('Woohoo, time for the weekend!')
+# else:
+#     print('Not quite there yet.')
+
+#     x = 10
+#     if x > 50:
+#     	print("bigger than 50")
+#     else:
+#     	print("smaller than 50")
+
+# empty_dict = {}
+# new_person = {'name': 'John', 'age': 38, 'weight': 160.2, 'has_glasses': False}
+# new_person['name'] = 'Jack'	# updates if the key exists, adds a key-value pair if it doesn't
+# new_person['hobbies'] = ['climbing', 'coding']
+# print(new_person)	
+# # output: {'name': 'Jack', 'age': 38, 'weight': 160.2, 'has_glasses': False, 'hobbies': ['climbing', 'coding']}
+# w = new_person.pop('weight')	# removes the specified key and returns the value
+# print(w)		# output: 160.2
+# print(new_person)	
+# # output: {'name': 'Jack', 'age': 38, 'has_glasses': False, 'hobbies': ['climbing', 'coding']}  
+# # 
+
+# print(type(2.63))		# output: <class 'float'>
+# print(type(new_person))		# output: <class 'dict'>
+name = "Zen"
+print("My name is", name)
+
+name = "Zen"
+print("My name is " + name)
+
+	# output: TypeError
+print("Hello " + str(42))		# output: Hello 42
+
+first_name = "Zen"
+last_name = "Coder"
+age = 27
+print(f"My name is {first_name} {last_name} and I am {age} years old.")
+
+first_name="Walid"
+last_name="guetat"
+age=33
+print(f"My name is {first_name} {last_name} d'age {age}")
+
+a=15
+b=20
+print(f"sum {a} and {b} is equal {a+b}")
+
+first_name = "Zen"
+last_name = "Coder"
+age = 27
+print("My name is {} {} and I am {} years old.".format(first_name, last_name, age))
+# output: My name is Zen Coder and I am 27 years old.
+print("My name is {} {} and I am {} years old.".format(age, first_name, last_name))
+# output: My name is 27 Zen and I am Coder years old.
+
+
+
+
+
+
+
