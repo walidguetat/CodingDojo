518253599d90bf5d853d356e284664841368da12
walidguetat <gtetwalid@yahoo.fr>
2024-02-23 15:24:45 +0100

    baskett ball dictionnaries

---
+++ Python/Python/Week 1/Day 3/Practice/baskett ball dictionnaries/baskett_ball_dictionarie.py
@@ -0,0 +1,70 @@
+
+
+# Challenge 1: Update the Constructor 
+
+class Player:
+    def __init__(self,play_one):
+        self.name = play_one["name"]
+        self.age = play_one["age"]
+        self.position = play_one["position"]
+        self.team = play_one["team"]
+        
+    def display_player(self):
+        print(f"Name : {self.name} \nAge : {self.age} \nPosition : {self.position} \nTeam : {self.team} ")
+        return self
+    def add_new_player(self,play_one):
+        new_team=[]
+        new_team.append(play_one)
+        return self
+    def display_list_player(self,new_team):
+        for i in new_team:
+            print(f" Team : {new_team[i]} \n")
+        return self
+
+
+
+
+print("\n------------------------------------------\n")
+
+# Challenge 2: Create instances using individual player dictionaries.
+
+player_one1={"name":"kevin Durant","age":34,"position":"small forward","team":"Brooklyn Nets"}
+
+player1=Player(player_one1)
+player1.display_player()
+
+print("\n------------------------------------------\n")
+
+player_one2={"name":"Jason Tatum","age":24,"position":"small forward","team":"Boston Czltics"}
+
+player1=Player(player_one2)
+player1.display_player()
+
+print("\n------------------------------------------\n")
+
+player_one3={"name":"Kyrie Irving","age":32,"position":"Point Guard","team":"Brooklyn Nets"}
+
+player1=Player(player_one3)
+player1.display_player()
+
+print("\n------------------------------------------\n")
+
+# Challenge 3: Make a list of Player instances from a list of dictionaries
+
+new_team=[]
+
+player_one1={"name":"Walid guetat","age":20,"position":"milieu","team":"Taraji"}
+
+player1=Player(player_one1)
+player1.add_new_player(player_one1)
+player1.display_list_player(new_team)
+
+
+# bonnus  Next times 
+
+
+
+
+
+
+
