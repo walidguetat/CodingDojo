6dee654196d75fa20a606e4cf657cc59e600a334
walidguetat <gtetwalid@yahoo.fr>
2024-03-01 11:46:17 +0100

    Counter

---
+++ Python/Python/Week 1/Day 5/Core/counter/server.py
@@ -0,0 +1,35 @@
+
+
+
+
+from flask import Flask, render_template, request, redirect, session
+
+
+app = Flask(__name__) 
+app.secret_key = 'keep it secret, keep it safe' # set a secret key for security purposes
+
+
+@app.route('/')          
+def displayCounter():
+    if 'counter' not in session:
+            session['counter']=1
+    return render_template('counter.html')  
+
+@app.route('/increment',methods=['POST'])
+def onClickIncrementCounter():
+    if 'counter' in session:
+            session['counter']+=1
+    return redirect('/')
+
+@app.route('/clear',methods=['POST'])
+def onReset():
+    session.pop('counter')
+    return redirect('/')
+
+
+
+
+
+if __name__=="__main__":      
+    app.run(debug=True,port=3200)    
+
