f66fa66824771ba882489d5e8443498c376d9b6a
Walid Guetat <63478267+walidguetat@users.noreply.github.com>
2024-05-14 13:19:31 +0100

    First program java

---
+++ ـJava/Week1/Day1/Practice/First Program Java/firstProgramJava.java
@@ -0,0 +1,9 @@
+public class firstProgramJava {
+	public static void main(String[] args) {
+		System.out.println("My name is Walid Guetat");
+		System.out.println("i'm 33 years old ");
+		System.out.println("My hometown is Tunisia, Tunis ");
+
+
+	}
+}
